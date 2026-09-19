e49518f8b9ffea4319d3c35b48f3b26e7b082fa9
codeTester190 <joaquim.augusto.4320@gmail.com>
2026-09-18 21:36:52 -0300

    feat: implmentando docs

--- src/main/java/com/br/api_logistica/controller/ClienteController.java
+++ src/main/java/com/br/api_logistica/controller/ClienteController.java
@@ -3,14 +3,28 @@ package com.br.api_logistica.controller;
 import com.br.api_logistica.dto.cliente.ClienteRequest;
 import com.br.api_logistica.dto.cliente.ClienteResponse;
 import com.br.api_logistica.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
+/**
+ * Controller REST responsavel pelos endpoints relacionados ao recusso cliente
+ */
+@Tag(
+        name = "Clientes",
+        description = "Operações relacionadas ao gerenciamento de clientes"
+)
 @RestController
 @RequestMapping("/v1/api/clientes")
 public class ClienteController {
@@ -21,11 +35,38 @@ public class ClienteController {
         this.service = service;
     }
 
+    /**
+     * Lista todos os clientes cadastrados
+     * @return Lista de clientes
+     */
+    @Operation(
+            summary = "Lista de clientes",
+            description = "Retorna todos os clientes cadastrados"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Clientes retornados com sucesso"
+    )
     @GetMapping
     public ResponseEntity<List<ClienteResponse>> listar(){
         return ResponseEntity.ok(service.listar());
     }
 
+    @Operation(
+            summary = "Busca cliente por ID",
+            description = "Retorna os detalhes de um cliente específico com base no seu identificador único"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Cliente encontrado com sucesso"
+            ),
+            @ApiResponse (
+                    responseCode = "404",
+                    description = "Cliente não encontrado",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+            )
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ClienteResponse> buscarPorId(
             @PathVariable Long id){
